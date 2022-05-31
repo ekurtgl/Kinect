cc8c0d0617fc14dac74d33d5ab24c6aa80efb122
ekurtgl <66868163+ekurtgl@users.noreply.github.com>
2022-05-30 23:07:11 -0500

    Add files via upload

---
+++ Postprocessing/read_hand_skeleton.m
@@ -0,0 +1,27 @@
+clc; clear; close all;
+
+datapath = 'C:\Users\emrek\PycharmProjects\Kinect_Skeleton\skeletons\*.txt';
+plot_on = true;
+files = dir(datapath);
+
+for i = 1:length(files)
+    figure(1);
+    clf;
+    hold on; 
+    
+    fid = fopen([files(i).folder '\' files(i).name]);
+    content = cell(1);
+    content{1} = fgetl(fid);
+    cnt = 2;
+    while ischar(content{cnt-1})
+        content{cnt} = fgetl(fid);
+        cnt = cnt + 1;
+    end
+    fclose(fid);
+    
+    if isempty(content)
+        continue
+    end
+    content = content(1:end-1);
+    [first_hand_data, second_hand_data, connections] = plot3D_hand(content, plot_on);
+end
